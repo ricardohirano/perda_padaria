bae1642dfa0a0963efa675038f72618db3100cdd
Ricardo Kaeriyama Hirano <ricardohiranorgt@gmail.com>
2024-10-15 20:57:09 -0300

    aula02

--- index.js
+++ index.js
@@ -2,7 +2,29 @@
 const express = require("express");
 // Inicioando o express
 const app = express(); 
+// indicar  EJS
+app.set('view engine', 'ejs');
+app.get ("/", function(req, res){
+    res.render("index")
+})
+app.get("/perfil", function (req, res){
+    res.render("perfil")
+})
 
+// ROTA PEDIDO
+app.get("/pedidos", function( req, res){
+    var produtos = [
+        { nome: "Celular", preço: 3000},
+        { nome: "Computador", preço: 4000},
+        { nome: "Tablet", preço: 1500},
+        { nome: "Fone", preço: 200},
+    ]
+    res.render ("pedidos", {
+        produtos: produtos
+    })
+})
+
+/*
 //rota principal
 app.get("/", function(req, res){
     res.send("<h1>Bem vindo ao meu site!</h1>")
@@ -19,7 +41,7 @@ app.get("/perfil/:nome", function(req, res){
     res.send(`<h2>ola, ${nome}! <br>Seja bem vindo!</h2>`)
     })
 */
-
+/*
 // rota perfil paramento nao obrigatorio
 
 app.get("/perfil/:nome?", function(req, res){
@@ -31,7 +53,7 @@ app.get("/perfil/:nome?", function(req, res){
     }
     
     })
-
+*/
 //inciando servidor
 app.listen(8080, function(erro){
     if(erro){
