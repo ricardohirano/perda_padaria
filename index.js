cb9f12372efa28d30dba8d41a9dcfe3a4033dc20
Ricardo Kaeriyama Hirano <ricardohiranorgt@gmail.com>
2024-10-15 20:03:39 -0300

    aula 01.2

--- index.js
+++ index.js
@@ -7,10 +7,29 @@ const app = express();
 app.get("/", function(req, res){
     res.send("<h1>Bem vindo ao meu site!</h1>")
 })
-// rota perfil
+/* rota perfil sem paramentro
 
 app.get("/perfil", function(req, res){
-    res.send("<h1> Perfil do Usuario</h1>")
+    res.send(`<h2>Seja bem vindo!</h2>`)
+    })*/
+/* rota perfil paramento fixo
+
+app.get("/perfil/:nome", function(req, res){
+    var nome = req.params.nome
+    res.send(`<h2>ola, ${nome}! <br>Seja bem vindo!</h2>`)
+    })
+*/
+
+// rota perfil paramento nao obrigatorio
+
+app.get("/perfil/:nome?", function(req, res){
+    var nome = req.params.nome
+    if(nome){
+        res.send(`<h2>ola, ${nome}! <br>Seja bem vindo!</h2>`);
+    } else {
+        res.send(`<h2> Digite o seu nome para acessar seu perfil</h2>`);
+    }
+    
     })
 
 //inciando servidor
